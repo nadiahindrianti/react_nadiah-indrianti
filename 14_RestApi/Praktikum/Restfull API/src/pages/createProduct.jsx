2b7486df1a8d7e071b90dc8afc7bb4207fccec66
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2023-10-15 03:21:15 +0700

    Update Restfull API

---
+++ 14_RestApi/Praktikum/Restfull API/src/pages/createProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from '../components/Content';
+import Header from '../components/Header';
+import Table from "../components/Table";
+
+export default function createProduct() {
+  return (
+    <div>
+        <Header />
+        <Content />
+        <Table />
+    </div>
+  )
+}
